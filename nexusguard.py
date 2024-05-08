1900b61a8f8f9d78956235c4dfde59206c86d2e8
lucasgaleano <l.a.galeano2@gmail.com>
2024-05-08 16:52:34 -0300

    initial commit

---
+++ nexusguard.py
@@ -0,0 +1,59 @@
+from nexusguardClient import nexusguardClient
+import configparser
+import time
+import json
+
+
+config = configparser.ConfigParser()
+config.read('nexusguard.conf')
+
+n = nexusguardClient(appID=config['nexus']['appID'], appSecret=config['nexus']['appSecret'])
+
+try:
+    n.connect()
+except Exception as e:
+    print(e)
+    exit()
+
+
+siteID = config['nexus']['siteID']
+lastAlert = ''
+
+while True:
+
+    alerts = n.get_alerts(siteID)['result']
+    for alert in alerts['events']:
+        if lastAlert == alert['alert_id']:
+            break
+        alert['app'] = 'nexusguard'
+        print(json.dumps(alert))
+
+    lastAlert = alerts['events'][0]['alert_id']
+
+    time.sleep(60)
+
+"""
+{
+  "code": 0,
+  "msg": "Success.",
+  "result": {
+    "total": 38,
+    "events": [
+      {
+        "severity": 3,
+        "attack_type": "Abnormal",
+        "end_time": 1715145300,
+        "severity_level": "Auto Mitigation",
+        "profile_desc": "AS9999",
+        "max_pps": 275165,
+        "duration": 721,
+        "site_name": "company_name",
+        "start_time": 1715144580,
+        "profile_name": "AS111111",
+        "site_ip": "134.44.233.32",
+        "alert_id": "EVENT-123456789ABDCDEF",
+        "p_type": 1,
+        "status": 0,
+        "max_bps": 1308675998
+      },
+"""
---
+++ nexusguardClient.py
@@ -0,0 +1,58 @@
+
+import requests
+from dataclasses import dataclass, field
+from datetime import datetime, timedelta
+
+
+@dataclass
+class nexusguardClient():
+    
+    appID: str
+    appSecret: str
+    urlOauth: str = "https://api.nexusguard.com/oauth/credit/token"
+    urlAPI:str = 'https://api.nexusguard.com/api'
+    token: str = ''
+    tokenExpires: datetime = field(init=False)
+
+    def connect(self):
+        data = {'app_id':self.appID, 'secret':self.appSecret, 'grant_type':'client_credential'}
+        response = requests.post(self.urlOauth, data=data)
+        codeStatus = response.json()['code']
+        if codeStatus != 0:
+            raise Exception(response.json()['msg'])
+        result = response.json()['result']
+
+        self.token = result['access_token']
+        self.tokenExpires = datetime.now() + timedelta(seconds=int(result['expires_in']))
+        print('token generated')
+
+    def renew_token(self):
+        if datetime.now() > self.tokenExpires:
+            self.connect()
+            print('token renewed')
+
+    def make_request(self, endpoint, extraData=None):
+        if not extraData:
+            extraData = {}
+
+        self.renew_token()
+        data = {'access_token':self.token} | extraData
+        url = self.urlAPI + endpoint
+        return requests.get(url, data).json()['result']
+        
+    def get_alerts(self, siteID, numLastAlerts:int=10):
+        extraData = {'num':numLastAlerts}
+        return self.make_request(f'/specp/op/dashboard/site/{siteID}/ddos_events', extraData)
+
+    def get_last_alert(self, siteID):
+        return self.get_alerts(siteID, 1)
+    
+    def get_alert_detail(self, siteID, alertID):
+        return self.make_request(f'/specp/op/dashboard/site/{siteID}/ddos_event/{alertID}')
+
+    def get_sites(self):
+        return self.make_request(f'/specp/op/sites')
+        
+
+
+
